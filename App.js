01cafeb35253a7b6c29bf3146f31307360c123ea
Mohammad Areeb Siddiqui <mohammadareebs@gmail.com>
2020-12-09 19:30:27 +0500

    Remove unused imports

--- App.js
+++ App.js
@@ -9,12 +9,6 @@
 import React  from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './src/components/LoginScreen';
-import SplashScreen from './src/components/SplashScreen';
-import HomeScreen from './src/components/HomeScreen';
-import SignupScreen from './src/components/SignupScreen';
-import { SCREENS } from './src/utils/constants';
 
 import rootReducer from './src/redux/reducers';
 
@@ -23,14 +17,9 @@ import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { RootNavigator } from './src/navigation/RootNavigator';
 
-
-const Stack = createStackNavigator();
 const store = createStore(rootReducer, {}, applyMiddleware(thunk, logger));
-
-const Drawer = createDrawerNavigator();
 
 const Main = () => {
   return (
